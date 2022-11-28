5df5a751a8fc963155adc782ac91426fbb36eceb
Armaan Noor Singh Brar <82953012+ArmaanBrar2001@users.noreply.github.com>
2022-11-28 23:26:16 +0530

    day-2 added

---
+++ day-2.py
@@ -0,0 +1,8 @@
+# Description: Take an input character from user and check whether it is an alphabet or not.
+
+a = input('Enter the character: ')
+
+if a.isalpha():
+    print('Alphabet')
+else:
+    print('Not an Alphabet')
