ddc1bf8e6e21ff8e07f762f7ea1ad4d4dc2a73a3
Armaan Noor Singh Brar <82953012+ArmaanBrar2001@users.noreply.github.com>
2022-11-28 23:26:58 +0530

    day-4 added

---
+++ day-4.py
@@ -0,0 +1,11 @@
+# Get an input number from the user and check whether it is a positive or negative number.
+
+a = int(input())
+
+if a > 0:
+    print('Positive Number')
+elif a < 0:
+    print('Negative Number')
+else:
+    print('Neither Positive nor Negative')
+
